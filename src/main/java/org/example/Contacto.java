f9a2a7380f058e3997cee73cfac1711f685e8af4
Manuel <5828067@alu.murciaeduca.es>
2024-03-08 12:43:13 +0100

    Encapsular los dos atributos de la clase Contacto.

--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -7,9 +7,26 @@ import java.util.List;
  * Clase que representa un contacto en una agenda.
  */
 class Contacto {
+
     private String name;
     private List<String> phones;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<String> phones) {
+        this.phones = phones;
+    }
+}
 /**
  * Constructor de la clase Contacto.
  *
