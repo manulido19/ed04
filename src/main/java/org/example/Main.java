e8e3822f2ec63f5d0701739538955edfa5baa0dd
Manuel <5828067@alu.murciaeduca.es>
2024-03-08 12:39:19 +0100

    Introduce el método initAgenda, que englobe las sentencias de la clase Main que operan con el objeto agenda1.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,7 @@ public class Main {
         // Crear una nueva agendaContactos.
         Agenda agendaContactos = new Agenda();
 
-        // Añadir contactos a la agendaContactos.
-        agendaContactos.addContact("John Doe", "1234567890");
-        agendaContactos.addContact("Jane Doe", "9876543210");
-        agendaContactos.addContact("Mary Jane", "1122334455");
+        initAgenda(agendaContactos);
 
         // Imprimir todos los contactos de la agendaContactos.
         System.out.println("Todos los contactos: ");
@@ -29,5 +26,11 @@ public class Main {
         for (Contacto c : agendaContactos.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
+    }
+    public static initAgenda(Agenda agendaContactos){
+        // Añadir contactos a la agendaContactos.
+        agendaContactos.addContact("John Doe", "1234567890");
+        agendaContactos.addContact("Jane Doe", "9876543210");
+        agendaContactos.addContact("Mary Jane", "1122334455");
     }
 }
